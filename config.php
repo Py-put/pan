736be0e4d3bdae3a3fe617343a0ceb0503aee180
Py-put <2481047285@qq.com>
2025-03-10 15:58:16 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,10 @@
 <?php
 /*数据库配置*/
 $dbconfig=array(
-    'host' => 'localhost', //数据库服务器
-    'port' => 3306, //数据库端口
-    'user' => '', //数据库用户名
-    'pwd' => '', //数据库密码
-    'dbname' => '' //数据库名
+    'host' => 'app-702450f9-d7ab-471f-bd42-6ec9118453f1-do-user-19702602-0.k.db.ondigitalocean.com', //数据库服务器
+    'port' => 25060, //数据库端口
+    'user' => 'ab', //数据库用户名
+    'pwd' => 'AVNS_lYgIZoj-JRfn-2FcRo8', //数据库密码
+    'dbname' => 'ab' //数据库名
 );
     
